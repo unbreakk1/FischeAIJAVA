ccfb4957c2872018938cd93748af5d56dad08c97
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-11 15:15:41 +0100

    add string reverse and palindrome

--- src/main/java/unbreakk1/Main.java
+++ src/main/java/unbreakk1/Main.java
@@ -23,4 +23,26 @@ public class Main
         return reversed.reverse().toString();
     }
 
+    public static boolean isPalindrome(String input)
+    {
+        if (input == null || input.isEmpty())
+            return false;
+
+
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right)
+        {
+            // Compare characters at the two ends
+            if (input.charAt(left) != input.charAt(right))
+                return false; // If characters differ, it's not a palindrome
+
+            left++;
+            right--;
+        }
+
+        return true; // String is a palindrome
+
+    }
 }
